e2dc339efecae05f1309d879575a2a802869d743
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-13 09:25:20 +0530

    Add files via upload

---
+++ data_structures.py
@@ -0,0 +1,29 @@
+list = [1,1,2,2,3,4]
+print(list)
+a="i am learning how to use computer, computer is a device used to process the given data and present an output "
+b=a.split()
+print(b)
+c=b.count('computer')
+print(c)
+b.append("hello")
+print(b)
+
+#dictionary
+dict={"a":"apple", "b":"banana", "c":"cat"}
+print(dict)
+print(dict["a"])
+print(len(dict))
+x=dict.values()
+print(x)
+
+set=("a","b","c")
+set.remove("a")
+print(set)
+
+
+
+
+
+
+
+
