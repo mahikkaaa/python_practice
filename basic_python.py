702c86fd2af7a2ba6560dc9a9dcb213a50c89ca5
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-13 08:47:05 +0530

    Add files via upload

---
+++ basic_python.py
@@ -0,0 +1,74 @@
+a=float(input("enter float type number"))
+b=int(a)
+print(b)
+
+c=int(input("enter integer type number"))
+d=float(c)
+print(d)
+
+print("""As per the constitution in section 97 IPC enables use of force in self defense
+only to protect one’s body and that of others from the actions of assaulting criminals/offenders,
+whose actions reasonably cause the apprehension of death or grievous hurt.
+In such circumstances the action taken by the police is fully protected by law.""")
+
+print("As per the constitution in section 97 IPC enables use of force in self defense \n only to protect one’s body and that of others from the actions of\tassaulting criminals/offenders, whose actions reasonably cause the apprehension of death or grievous hurt. In such circumstances "
+      #"the action taken by the police is fully protected by law.")
+
+print(1+2)
+a="1"
+b="2"
+print(a+b)
+
+a=input("enter your first name")
+b=input("enter your last name")
+c=a+b
+print(c)
+
+a=("hi,i am learning python")
+b=a.title()
+print(b)
+
+a=("Hi,I Am Learning Python")
+b=a.lower()
+print(b)
+
+a=("Hi,I Am Learning Python")
+b=a.islower()
+print(b)
+
+a=("Hi,I Am Learning Python")
+b=a.casefold()
+print(b)
+
+c=("Hi,I Am Learning Python")
+d=c.split()
+print(d)
+
+c=("Hi,I Am Learning Python")
+d=c.strip()
+print(d)
+
+c=("Hi,I Am Learning Python")
+d=c.partition("Am")
+print(d)
+
+a=("Hi,I Am Learning Python, Python is easy to learn")
+b=a.count("Python")
+print(b)
+
+c=("Hi,I Am Learning Python")
+d=c.encode()
+print(d)
+
+c=("Hi,I Am Learning Python")
+d=c.endswith("n")
+print(d)
+
+c=("Hi,I Am Learning Python")
+d=c.replace("Python","java")
+print(d)
+
+
+
+
+
