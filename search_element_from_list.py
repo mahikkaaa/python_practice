5f263054b5d2ce7a93d2c0a96af7ec93dc93fb1c
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-13 11:12:12 +0530

    Update search_element_from_list.py

--- search_element_from_list.py
+++ search_element_from_list.py
@@ -1,3 +1,4 @@
+#WHILE LOOP
 #program to count the appearance of an element in a list
 a=["aa", "aa", "aa", "bb", "bb", "bb", "bb", "bb", "bb", "bb", "vv", "cc"]
 print(a)
@@ -33,4 +34,43 @@ while(a<b):
     a=a+1
 print("the entered word has appeared", c, "times")
 
+
+#FOR LOOP
+#program to make sub list from list without using split function
+a=["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh"]
+b=len(a)
+print(b)
+d=[]
+e=[]
+for c in range(b):
+    f=c%2
+    #print(f)
+    if(f==0):
+        d.append(a[c])
+    elif(f!=0):
+        e.append(a[c])
+    c=c+1
+print(d)
+print(e)
+
+
+#program to make string to sub list from list without using split function
+x = ("""A computer is a machine that can be instructed to carry out sequences of arithmetic or logical
+ operations automatically via computer programming. modern computer have the
+  ability to follow generalized sets of operations, called programs. these programs
+   enable computer to perform an extremely wide range of tasks. programs are very useful, programs help a person learn better""")
+y=x.split(" ")
+print(y)
+z=len(y)
+print(z)
+a=[]
+b=[]
+for c in range(z):
+    d=c%2
+    if(d==0):
+        a.append(y[c])
+    elif(d!=0):
+        b.append(y[c])
+print(a)
+print(b)
 
