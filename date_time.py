865398b558db04dadf86041991bb10fc43431550
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-14 10:54:19 +0530

    Add files via upload

---
+++ date_time.py
@@ -0,0 +1,12 @@
+#inbuilt functions
+import datetime as dt
+x=dt.datetime.now()
+print("current time is",x)
+
+#printing
+import datetime as dt
+x=dt.datetime.now()
+print(x.strftime("%B"))
+#%A - weekday(full), %B- MONTH IN ALPHABET, %a-weekday(short), %b- month name(short), %w- no. of days, %d - day of month, %m - month as numeric, %y = year short (18), %Y - year full (2018)
+
+
